34a7ba705bf4364e8422eecf7863cdcff7275511
CAMHub PublishBot <octravis@us.ibm.com>
2019-08-16 19:12:52 +0000

    Automated CAMHub::publish on 2019-08-16 19:12:52 UTC

--- openstack/terraform/output.tf
+++ openstack/terraform/output.tf
@@ -1,3 +1,8 @@
+# Generate a random id to maintian output object uniqueness
+resource "random_id" "clusterid" {
+  byte_length = "2"
+}
+
 output "ibm_cloud_private_admin_url" {
   value = "https://${element(values(var.master_hostname_ip),0)}:8443"
 }
@@ -17,3 +22,36 @@ output "ibm_cloud_private_master_ip" {
 output "ibm_cloud_private_proxy_hostname" {
   value = "${element(keys(var.proxy_hostname_ip),0)}.${var.vm_domain}"
 }
+
+output "ibm_cloud_private_custer_ca_domain" {
+	value = "${var.icp_cluster_name}.${var.vm_domain}"
+}
+
+output "cloud_connection_name"{
+	value = "${var.icp_cluster_name}${random_id.clusterid.hex}Connection" 
+}
+
+output "registry_config_do_name"{
+	value = "${var.icp_cluster_name}${random_id.clusterid.hex}RegistryConfig" 
+}
+
+output "registry_ca_cert"{
+  value = "${module.icp_config_output.registry_ca_cert}"
+}  
+
+output "icp_install_dir"{
+  value = "${camc_scriptpackage.get_home_dir.result["stdout"]}/ibm-cloud-private-x86_64-${var.icp_version}/cluster"
+}
+
+#output "kube_config_do_name"{
+#	value = "${var.icp_cluster_name}${random_id.clusterid.hex}KubeConfig" 
+#}
+
+#output "cluster_config"{
+#  value = "${module.icp_config_output.cluster_kube_config}"
+#}
+
+#output "cluster_ca_certificate"{
+#  value = "${module.icp_config_output.cluster_kube_config_ca_cert_data}"
+#}
+
